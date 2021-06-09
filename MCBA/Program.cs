abc3c42a14f5214734e688d3c9326a4637857b4e
s3807720 <s3807720@users.noreply.github.com>
2021-06-09 21:15:19 +1000

    First commit for the start of assignment!

--- MCBA/Program.cs
+++ MCBA/Program.cs
@@ -6,7 +6,7 @@ namespace MCBA
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Testing commits!!");
         }
     }
 }
