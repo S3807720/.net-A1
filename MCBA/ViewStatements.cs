deef406f6524e207a3d34e9f17849f367e1950a3
s3807720 <s3807720@users.noreply.github.com>
2021-06-22 13:36:58 +1000

    Added statements

---
+++ MCBA/ViewStatements.cs
@@ -0,0 +1,71 @@
+﻿using MCBA.Managers;
+using MCBA.Models;
+using MiscellaneousUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCBA
+{
+    class ViewStatements
+    {
+        public ViewStatements(Account account)
+        {
+            var transactions = account.transactions;
+            var start = 0; 
+            var end = 3;
+            var menuChoice = false;
+            Console.WriteLine($"Account #{account.accountNumber} has a balance of ${account.balance}.");
+            while (menuChoice == false)
+            {
+                for (int i = start; transactions.Count > end? end >= i : transactions.Count > i; i++)
+                {
+                    //print each transaction for the account
+                    Console.WriteLine(transactions[i]?.ToString().Replace("@", Environment.NewLine));
+                    Console.WriteLine();
+                }
+
+                //show menu options based on transactions displayed
+                var menu = "";
+                if (transactions.Count > end)
+                {
+                    menu += "Q. Next page";
+                }
+                if (start > 3)
+                {
+                    menu += "\nW. Previous page";
+                }
+                menu += "\nE. Back.";
+                
+                //if transaction count is lower than the maximum page amount, change end to that
+                if (end > transactions.Count) end = transactions.Count;
+                Console.WriteLine($"{start+1}-{end} of {transactions.Count}\n{menu}");
+                var input = Console.ReadLine().ToUpper();
+                //enable page functions based on amount of transactions and what page the user is on
+                if (transactions.Count > end)
+                {
+                    if (input == "Q")
+                    {
+                        start += 4;
+                        end += 4;
+                    }
+                    else if (input == "W" && start > 3)
+                    {
+                        start -= 4;
+                        end -= 4;
+                    }
+                }                
+                if (input == "E")
+                {
+                    return;
+                } else
+                {
+                    Console.WriteLine($"{input} is not a menu option.");
+                }
+            }
+
+        }
+    }
+}
